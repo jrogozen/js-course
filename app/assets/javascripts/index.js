7173c3d797eadbcb73b11c623f561f1faec3fb7f
Jon Rogozen <jon.rogozen@gmail.com>
2014-10-13 13:52:14 -0700

    refactor and clean up functions

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -22,68 +22,75 @@ $(document).ready (function () {
     });
   };
 
+
+  var checkAnswer = function (quiz_id, question_id, n, score, qlength) {
+    $('.answer-form').submit(function (e) {
+      e.preventDefault();
+      var answer = $('input[type="radio"]:checked').val();
+
+      // process answer
+      $.get("/quizzes/" + quiz_id + "/questions/" + question_id + "/check?answer=" + answer , function (data) {
+       if (data.correct === true) {
+          score ++;
+          $('.answer-validation').append("<p>Correct!</p>");
+        } else if (data.correct === false) {
+          $('.answer-validation').append("<p>Incorrect</p>");
+        }
+      }) 
+        .done(function () {
+          // show next question
+          if (n < qlength-1) {
+            setTimeout(function () {
+              n ++;
+              playQuiz(quiz_id, n, score);
+            }, 1000);
+          } else {
+            // show score
+            $('.answer-form').find('input').attr('disabled', 'disabled');
+            $('.score-summary').append("<p>You got " + score + " out of " + qlength + " questions correct.</p>")
+          }
+        });
+
+    });
+  }
+
   var playQuiz = function (quiz_id, n, score) {
 
+    $.get("/quizzes/" + quiz_id + "/questions", function (data) {
 
-    $.get("/quizzes/" + quiz_id, function (data) {
+      $('#questions-container').empty();
 
+      var qlength = data.length
 
-    })
-      .done(function () {
-        $.get("/quizzes/" + quiz_id + "/questions", function (data) {
-          $('#questions-container').empty();
-          // console.log(data);
+      var view = {
+        answer: data[n].answer,
+        choices: data[n].choices.split(";"),
+        question: data[n].question,
+        question_type: data[n].question_type,
+        template: _.template($(".quiz-question").html())
+      }
 
-          var view = {
-            answer: data[n].answer,
-            choices: data[n].choices.split(";"),
-            question: data[n].question,
-            question_type: data[n].question_type,
-            template: _.template($(".quiz-question").html())
-          }
+      $('#questions-container').append(view.template());
 
-            $('#questions-container').append(view.template());
+      checkAnswer(quiz_id, data[n].id, n, score, qlength);
 
-            $('.answer-form').submit(function (e) {
-              e.preventDefault();
-              var answer = $('#answer').val();
-
-              // process answer
-              $.get("/quizzes/" + quiz_id + "/questions/" + data[n].id + "/check?answer=" + answer , function (data) {
-               if (data.correct === true) {
-                  score ++;
-                  $('.answer-validation').append("<p>Correct!</p>");
-                } else if (data.correct === false) {
-                  $('.answer-validation').append("<p>Incorrect</p>");
-                }
-              }) 
-                .done(function () {
-                  // show next question
-                  if (n < data.length-1) {
-                    setTimeout(function () {
-                      n ++;
-                      playQuiz(quiz_id, n, score);
-                    }, 1000);
-                  } else {
-                    // show score
-                    $('.score-summary').append("<p>You got " + score + " out of " + data.length + " questions correct.</p>")
-                  }
-                });
-
-            });
-
-        })
-
-      }); 
+    });
 
   };
 
+
+
   showQuizzes();
+
+
+  // listener
 
   $('#quiz-select').find('.target').change(function () {
     // 
     var quiz_id = $("select option:selected").data('quiz-id');
     $('#questions-container').empty();
+
+    // start quiz
     playQuiz(quiz_id, 0, 0);
   });
 
